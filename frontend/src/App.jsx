c3683c8ce9c143c13f9883bef5a3cab8a9ac5577
Swostika Bajagain <sostikabajgai8@gmail.com>
2026-06-24 23:31:46 +0545

    Completed product list API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,59 +1,9 @@
-import { useEffect, useState } from "react";
+import ProductList from "../pages/ProductList";
 
-function App() {
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-
-    fetch("http://127.0.0.1:8000/api/products/")
-      .then(response => response.json())
-      .then(data => setProducts(data))
-      .catch(error =>
-        console.error("Error fetching data:", error)
-      );
-
-  }, []);
-
-  return (
-
-    <div className="min-h-screen bg-gray-100 text-gray-800">
-
-      <h1 className="text-3xl font-bold underline p-4">
-        Product List
-      </h1>
-
-      <div className="container mx-auto p-4">
-
-        {products.map(product => (
-
-          <div
-            key={product.id}
-            className="bg-white p-4 rounded shadow mb-4"
-          >
-
-            <h2 className="text-xl font-semibold">
-              {product.name}
-            </h2>
-
-            <p className="text-gray-600">
-              {product.description}
-            </p>
-
-            <p className="text-gray-800 font-bold">
-              ${product.price}
-            </p>
-
-          </div>
-
-        ))}
-
-      </div>
-
+function App(){
+  return(
+    <div>
+      <ProductList/>
     </div>
-
-  );
-
+  )
 }
-
-export default App;
