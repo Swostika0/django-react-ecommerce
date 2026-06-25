287247dd4557cbaf45b217946726ef1b09d44202
Swostika Bajagain <sostikabajgai8@gmail.com>
2026-06-25 20:59:57 +0545

    setup react router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import {BroserRouter as Router, Route, Routes} from 'react-router-dom';
 import ProductList from "../pages/ProductList";
+import ProductDetails from "../pages/ProductDetails";
+
 
 function App(){
   return(
-    <div>
-      <ProductList/>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element = {<ProductList/>}/>
+        <Route path="/product/:id" element = {<ProductDetails/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
